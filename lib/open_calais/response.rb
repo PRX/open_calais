45fea5cff1c838b591ae15ce49326be4b99084e3
Andrew Kuklewicz <andrew@beginsinwonder.com>
2020-07-25 17:31:44 -0400

    Refactor the parse method

--- lib/open_calais/response.rb
+++ lib/open_calais/response.rb
@@ -41,39 +41,68 @@ module OpenCalais
       r.each do |k,v|
         case v._typeGroup
         when 'topics'
-          self.topics << {:name => humanize_topic(v.name), :score => v.score.to_f, :original => v.name}
+          self.topics << {
+            :name => humanize_topic(v.name),
+            :score => v.score.to_f,
+            :original => v.name
+          }
         when 'socialTag'
-          self.tags << {:name => v.name.gsub('_', ' and ').downcase, :score => importance_to_score(v.importance)}
+          self.tags << {
+            :name => v.name.gsub('_', ' and ').downcase,
+            :score => importance_to_score(v.importance)
+          }
         when 'entities'
-          v.name = v.instances.first[:exact] if v.name.nil?
-          item = {:guid => k, :name => v.name, :type => transliterate(v._type).titleize, :score => v.relevance}
-
-          instances = Array(v.instances).select{|i| i.exact.downcase != item[:name].downcase }
-          item[:matches] = instances if instances && instances.size > 0
-
-          if OpenCalais::GEO_TYPES.include?(v._type)
-            if (v.resolutions && v.resolutions.size > 0)
-              r = v.resolutions.first
-              item[:name]      = r.shortname || r.name
-              item[:latitude]  = r.latitude
-              item[:longitude] = r.longitude
-              item[:country]   = r.containedbycountry if r.containedbycountry
-              item[:state]     = r.containedbystate if r.containedbystate
-            end
-            self.locations << item
-          else
-            self.entities << item
-          end
+          parse_entity(k, v)
         when 'relations'
-          item = v.reject{|key,val| key[0] == '_' || key == 'instances'} || {}
-          item[:type] = transliterate(v._type).titleize
-          self.relations << item
+          parse_relation(k, v)
         end
       end
 
       # remove social tags which are in the topics list already
-      topic_names = self.topics.collect{|topic| topic[:name].downcase}
-      self.tags.delete_if{|tag| topic_names.include?(tag[:name]) }
+      topic_names = self.topics.collect { |topic| topic[:name].downcase }
+      self.tags.delete_if { |tag| topic_names.include?(tag[:name]) }
+    end
+
+    def parse_entity(k, v)
+      if v.name.nil?
+        v.name = v.instances.first[:exact]
+      end
+
+      item = {
+        :guid => k,
+        :name => v.name,
+        :type => transliterate(v._type).titleize,
+        :score => v.relevance
+      }
+
+      instances = Array(v.instances).select { |i| i.exact.downcase != item[:name].downcase }
+      if instances && instances.size > 0
+        item[:matches] = instances
+      end
+
+      if OpenCalais::GEO_TYPES.include?(v._type)
+        self.locations << set_location_info(item, v)
+      else
+        self.entities << item
+      end
+    end
+
+    def set_location_info(item, v)
+      if (v.resolutions && v.resolutions.size > 0)
+        r = v.resolutions.first
+        item[:name]      = r.shortname || r.name
+        item[:latitude]  = r.latitude
+        item[:longitude] = r.longitude
+        item[:country]   = r.containedbycountry if r.containedbycountry
+        item[:state]     = r.containedbystate if r.containedbystate
+      end
+      item
+    end
+
+    def parse_relation(k, v)
+      item = v.reject { |key,val| key[0] == '_' || key == 'instances' } || {}
+      item[:type] = transliterate(v._type).titleize
+      self.relations << item
     end
   end
 end
